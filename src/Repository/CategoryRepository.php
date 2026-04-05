5b96419d8dc9e7b55924a704bc2bcc0eb80434b8
chvr4f <charaf2322004@gmail.com>
2026-04-05 09:33:07 +0100

    feat: add dynamic pages with Category entity and Twig templates

---
+++ src/Repository/CategoryRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Category;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Category>
+ *
+ * @method Category|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Category|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Category[]    findAll()
+ * @method Category[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CategoryRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Category::class);
+    }
+
+    public function findAllOrderedByDisplay(): array
+    {
+        return $this->createQueryBuilder('c')
+            ->orderBy('c.displayOrder', 'ASC')
+            ->addOrderBy('c.name', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findBySlug(string $slug): ?Category
+    {
+        return $this->findOneBy(['slug' => $slug]);
+    }
+}
