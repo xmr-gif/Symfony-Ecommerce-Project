6620f124aa37c88e7dfb992371b6f3420deeed79
oussama <hafidoss2004@gmail.com>
2026-05-24 19:52:05 +0100

    feat: add ShopController, SecurityController, ProfileController
    - ShopController: /shop (listing) and /product/{slug} (detail) routes
    - SecurityController: /login and /logout with AuthenticationUtils
    - ProfileController: /profile route protected by ROLE_USER
    - EcommerceController: inject ProductRepository for dynamic product data

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Redirect already-authenticated users to home
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_profile');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // This method is intercepted by the logout key on the firewall
+        throw new \LogicException('This method should not be reached.');
+    }
+}
